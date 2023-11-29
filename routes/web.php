f876cd48de49170768e90b202e943c5117fa8abb
TeddyFirman <teddyfirman902@gmail.com>
2023-11-29 19:50:49 +0700

    add breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -17,37 +18,13 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Homepage');
-})->name('/');
-
-Route::get('/exercise', function () {
-    $exerciseList = [
-        [
-            "title" => "Basic Grammar (J_GUP1)",
-            "slug" => "basic-grammar-1"
-        ],
-        [
-            "title" => "Basic Grammar (J_GUP2)",
-            "slug" => "basic-grammar-2"
-        ],
-    ];
-    return Inertia::render('Exercise/Index', [
-        "exercises" => $exerciseList
-    ]);
-})->name('exercise');
-
-Route::get('/exercise/{exercise}', function () {
-    return Inertia::render('Exercise/Detail');
-});
-
-Route::get('/welcome', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('Homepage', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
-});
+})->name('/');
 
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
@@ -59,4 +36,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
+Route::prefix('admin')->group(function () {
+    Route::get('dashboard', [DashboardController::class, 'index']);
+});
+
+require __DIR__.'/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-    './resources/js/**/*.jsx',
-    './public/**/*',
-    'node_modules/preline/dist/*.js',
-  ],
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.jsx',
+    ],
 
-  theme: {
-    extend: {
-      fontFamily: {
-        body: ['Assistant', 'sans-serif'],
-        head: ['Alegreya', 'serif'],
-      },
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
-  },
 
-  plugins: [require('@tailwindcss/forms'), require('preline/plugin')],
+    plugins: [forms],
 };
