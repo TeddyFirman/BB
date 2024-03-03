a984075608a6f5c9d786038e0361eeab3786bde8
TeddyFirman <teddyfirman902@gmail.com>
2024-03-03 09:27:40 +0700

    Updating: build

---
+++ public/build/assets/GuestLayout-7ea2676a.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-56248f72.js";function t({children:e}){return r.jsx("div",{className:"flex min-h-screen w-full flex-row items-center justify-center bg-gray-50",children:e})}export{t as G};
